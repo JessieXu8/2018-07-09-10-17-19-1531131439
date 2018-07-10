7e4ecfc001e26b5517519c54185d5ff0fddb6bbb
JESSIE XU (ITA-ISDC-ISD-OOCLL/ZHA) <XUJE8@OOCL.COM>
2018-07-10 16:44:51 +0800

    [jessie] completed the practice 9

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,49 @@
 package practice09;
 
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public void assignLeader(Student student){
+        if (student.getKlass().getNumber() == getNumber()){
+            leader = student;
+        }else{
+            System.out.print("It is not one of us.\n");
+        }
+
+    }
+
+    public void appendMember (Student student){
+        //if (student.getKlass().getNumber() == getNumber()){ }
+        student.setKlass(this);
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+    public Student getLeader(){
+        return leader;
+    }
+
+    public int getNumber() {
+        return number;
+
+    }
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName(){
+        if (number == 0){
+            return "";
+        }else {
+            return "Class " + number;
+        }
+    }
+
+
 }
