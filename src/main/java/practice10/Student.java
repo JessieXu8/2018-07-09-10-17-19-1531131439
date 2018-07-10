61ea33ab70f1338e679e9a5e4317fbeee33124c8
JESSIE XU (ITA-ISDC-ISD-OOCLL/ZHA) <XUJE8@OOCL.COM>
2018-07-10 19:56:06 +0800

    [jessie] completed the practice 10

--- src/main/java/practice10/Student.java
+++ src/main/java/practice10/Student.java
@@ -1,4 +1,30 @@
 package practice10;
 
-public class Student {
+public class Student extends Person{
+    private Klass klass;
+
+    Student(int id,String name, int age) {
+        super(id,name, age);
+    }
+
+    Student(int id,String name, int age, Klass klass) {
+        super(id,name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    public String introduce(){
+        if(klass.getLeader() != null &&  getId() == klass.getLeader().getId() ){
+            return super.introduce()+" I am a Student. I am Leader of "+klass.getDisplayName()+".";
+        }else {
+            return super.introduce() + " I am a Student. I am at " + klass.getDisplayName() + ".";
+        }
+    }
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,53 @@
 package practice10;
 
-public class Teacher {
+import java.util.LinkedList;
+
+public class Teacher extends Person{
+
+    private LinkedList<Klass> classes = new LinkedList<>();
+
+    public LinkedList<Klass> getClasses() {
+        return classes;
+    }
+
+    public void setClasses(LinkedList<Klass> classes) {
+        this.classes = classes;
+    }
+    public Teacher(int id,String name, int age) {
+        super(id,name, age);
+    }
+    public Teacher(int id,String name, int age, LinkedList<Klass> classes) {
+        super(id,name, age);
+        this.classes = (LinkedList<Klass>) classes.clone();
+    }
+
+    public String introduce(){
+        String teachOrNot = "No Class";
+        if(!(classes.isEmpty())) {
+            teachOrNot = "Class ";
+            for(Klass klass:classes){
+                String tmp = String.valueOf((klass.getNumber()));
+                teachOrNot+= tmp;
+                if(classes.getLast()!=klass)
+                    teachOrNot+= ", ";
+            }
+        }
+        return super.introduce() + String.format(" I am a Teacher. I teach %s.", teachOrNot);
+    }
+
+    public String introduceWith(Student student){
+        String teachOrNot = isTeaching(student)?"teach":"don't teach";
+        return super.introduce()+String.format(" I am a Teacher. I %s %s.", teachOrNot, student.getName());
+    }
+
+    public Boolean isTeaching(Student student){
+        boolean isTeach = false;
+        for (Klass klass: classes){
+            if(klass.equals(student.getKlass())){
+                isTeach = true;
+                break;
+            }
+        }
+        return isTeach;
+    }
 }
