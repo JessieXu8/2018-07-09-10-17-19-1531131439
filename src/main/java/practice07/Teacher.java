ea6a70aca986b402afe1bd0a3a3e1fc4981507c3
JESSIE XU (ITA-ISDC-ISD-OOCLL/ZHA) <XUJE8@OOCL.COM>
2018-07-10 14:55:14 +0800

    [jessie] finished the practice 7

--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,37 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person{
+
+    private Klass klass;
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public String introduce(){
+        if(klass == null){
+            return super.introduce()+" I am a Teacher. I teach No Class.";
+        }else {
+            return super.introduce() + " I am a Teacher. I teach " + klass.getDisplayName() + ".";
+        }
+    }
+
+    public String introduceWith(Student student){
+        if (student.getKlass() == this.getKlass()){
+            return super.introduce()+" I am a Teacher. I teach "+student.getName()+".";
+        }else {
+            return "My name is Tom. I am 21 years old. I am a Teacher. I don't teach "+student.getName()+".";
+        }
+    }
 }
